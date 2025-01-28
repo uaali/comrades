eaacca48fd9a8d24a3c704e3a75dc08a87fd9e7e
Ali Kuria <network4scholars@gmail.com>
2025-01-28 23:56:06 +0300

    searchbar n firewall

--- src/wrappers/AppCheckWrapper.tsx
+++ src/wrappers/AppCheckWrapper.tsx
@@ -7,16 +7,14 @@ import { app } from "@/lib/firebase/config";
 const AppCheckWrapper = () => {
   useEffect(() => {
     try {
-      if (process.env.NODE_ENV === "development") {
-        //@ts-ignore
-        self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+      if (process.env.NODE_ENV !== "development") {
+        initializeAppCheck(app, {
+          provider: new ReCaptchaV3Provider(
+            process.env.NEXT_PUBLIC_APPCHECK_KEY!
+          ),
+          isTokenAutoRefreshEnabled: true,
+        });
       }
-      initializeAppCheck(app, {
-        provider: new ReCaptchaV3Provider(
-          process.env.NEXT_PUBLIC_APPCHECK_KEY!
-        ),
-        isTokenAutoRefreshEnabled: true,
-      });
     } catch (error) {
       console.log(error);
     }
